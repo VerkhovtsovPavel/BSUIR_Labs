5d0ca512d903348a1693958f4b23f34ff64129a2
Pavel Verkhovtsov <verchpasha@gmail.com>
2014-09-06 19:21:03 +0300

    Add checkstyle

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args){
 		Rectangle rectangle = new Rectangle(10,10,50,50);
 		Line line = new Line(100,100, 500, 500);
-		Oval oval = new Oval(700, 700, 750, 750);
+		Oval oval = new Oval(200, 200, 250, 250);
 		Painter.addObjectInList(rectangle);
 		Painter.addObjectInList(line);
 		Painter.addObjectInList(oval);
