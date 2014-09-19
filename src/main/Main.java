b77c8837ffb99892cee6d7889414814ac7beec9f
Pavel Verkhovtsov <verchpasha@gmail.com>
2014-09-19 18:59:25 +0300

    Create method on each actions

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
@@ -20,36 +22,24 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		System.out.println("Shape drawing shell");
 
-		while(!commandLine.equals("Exit")){
+		while(true){
 			commandLine = new Scanner(System.in).nextLine();
 			String action = takeWord();
-			if (action.equals("Draw")){
+			
+			if (action.equals("Exit")){
+				System.exit(0);
+			}
+			else if (action.equals("Draw")){
 				Painter.drawObjects();
 			}
 			else if (action.equals("Add")){
-				String shapeName = takeWord();
-				ArrayList<Integer> paramList = parseParam();
-				
-				Class<?> shapeClass = Class.forName("shapes."+shapeName);
-				Constructor<?> shapeConstructor = shapeClass.getDeclaredConstructors()[0];
-				BaseShape shape = (BaseShape) shapeConstructor.newInstance(paramList);
-				Painter.addObjectInList(shape);
+				addShape();
 			}
-			
 			else if (action.equals("Save")){
-				String filePath = takeWord();
-				FileOutputStream fileStream = new FileOutputStream(filePath);
-				ObjectOutputStream serialazer = new ObjectOutputStream(fileStream);
-				serialazer.writeObject(Painter.getObjectList());
-				serialazer.flush();
-				serialazer.close();
+				saveObjectListInFile();
 			}
-			
 			else if (action.equals("Open")){
-				String filePath = takeWord();
-				FileInputStream fileStream = new FileInputStream(filePath);
-				ObjectInputStream deserialazer = new ObjectInputStream(fileStream);
-				Painter.raiseList( (ArrayList<BaseShape>) deserialazer.readObject());
+				readObjectListFromFile();
 			}
 		}
 	}
@@ -75,5 +65,43 @@ public class Main {
 			commandLine = "";
 		}
 		return word;
+	}
+	
+	private static void addShape() throws Exception{
+		String shapeName = takeWord();
+		ArrayList<Integer> paramList = parseParam();
+		Class<?> shapeClass = Class.forName("shapes."+shapeName);
+		Constructor<?> shapeConstructor = shapeClass.getDeclaredConstructors()[0];
+		BaseShape shape = (BaseShape) shapeConstructor.newInstance(paramList);
+		Painter.addObjectInList(shape);
+	}
+	
+	private static void saveObjectListInFile(){
+		String filePath = takeWord();
+		FileOutputStream fileStream;
+		try {
+			fileStream = new FileOutputStream(filePath);
+			ObjectOutputStream serialazer = new ObjectOutputStream(fileStream);
+			serialazer.writeObject(Painter.getObjectList());
+			serialazer.flush();
+			serialazer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readObjectListFromFile(){
+		String filePath = takeWord();
+		FileInputStream fileStream;
+		try {
+			fileStream = new FileInputStream(filePath);
+			ObjectInputStream deserialazer;
+			deserialazer = new ObjectInputStream(fileStream);
+			Painter.raiseList( (ArrayList<BaseShape>) deserialazer.readObject());
+		} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
