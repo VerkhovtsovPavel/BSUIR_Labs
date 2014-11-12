aaa756e44663b20253f46c7ba7f5f345dd5f0192
Pavel Verkhovtsov <verchpasha@gmail.com>
2014-11-13 00:35:54 +0300

    Add axis

--- src/graphics/GraphicsClass.java
+++ src/graphics/GraphicsClass.java
@@ -31,8 +31,9 @@ public class GraphicsClass extends JPanel {
 	public void paint(final Graphics g) {
 		drawProbabilityDensity(g, firstProbability, Color.RED);
 		drawProbabilityDensity(g, secondProbability, Color.BLUE);
+		axis(g);
 		g.setColor(Color.BLACK);
-		g.drawLine(median * X_SCALE, 0, median * X_SCALE, screenSize);
+		g.drawLine(median * X_SCALE+50, 0, median * X_SCALE+50, screenSize);
 	}
 
 	private void drawProbabilityDensity(Graphics g, Probability probability,
@@ -40,19 +41,27 @@ public class GraphicsClass extends JPanel {
 		g.setColor(color);
 		for (int i = 0; i < probability.randomVariable.size() - 1; i++) {
 			g.drawLine(
-					probability.randomVariable.get(i) * X_SCALE,
+					probability.randomVariable.get(i) * X_SCALE+50,
 					(int) (screenSize / (2.5 - probability.probalityOfClass) + screenSize / 2)
 							- (int) (probability
 									.probabilityDensity(probability.randomVariable
 											.get(i))
-									* Y_SCALE * probability.probalityOfClass),
-					probability.randomVariable.get(i + 1) * X_SCALE,
+									* Y_SCALE * probability.probalityOfClass)-50,
+					probability.randomVariable.get(i + 1) * X_SCALE+50,
 					(int) (screenSize / (2.5 - probability.probalityOfClass) + screenSize / 2)
 							- (int) (probability
 									.probabilityDensity(probability.randomVariable
 											.get(i + 1))
-									* Y_SCALE * probability.probalityOfClass));
+									* Y_SCALE * probability.probalityOfClass)-50);
 		}
+	}
+	
+	private static void axis(Graphics graphics){
+		graphics.drawLine(10, 20, 10, screenSize-10);
+		graphics.drawLine(10, screenSize-10, (int) (screenSize*2.3-30), screenSize-10);
+		graphics.setColor(Color.BLACK);
+		graphics.drawString("P(C1),P(C2)",20 , 20);
+		graphics.drawString("Variable value", (int) (screenSize*2.3-200), screenSize+10);
 	}
 
 	public static void buildGraph(final Probability firstProbability,
