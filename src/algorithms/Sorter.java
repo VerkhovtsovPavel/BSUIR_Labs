c25fd6f2423ab25d6d442308205a2d3292872457
Pavel Verkhovtsov <verchpasha@gmail.com>
2014-11-13 00:46:47 +0300

    Add Ploter and Sorter classes

---
+++ src/algorithms/Sorter.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+import java.util.ArrayList;
+
+import recipes.Recipe;
+
+public class Sorter {
+	private ArrayList<Recipe> allFoundRicepe;
+	private ArrayList<Recipe> firstGeneration;
+	private ArrayList<Recipe> secondGeneration;
+	private ArrayList<Recipe> thirdGenerartion;
+
+	public ArrayList<Recipe> getAllFoundRicepe() {
+		return allFoundRicepe;
+	}
+
+	public ArrayList<Recipe> getFirstGeneration() {
+		return firstGeneration;
+	}
+
+	public ArrayList<Recipe> getSecondGeneration() {
+		return secondGeneration;
+	}
+
+	public ArrayList<Recipe> getThirdGenerartion() {
+		return thirdGenerartion;
+	}
+}
