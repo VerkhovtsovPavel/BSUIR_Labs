7135c584efeb4a4338c8c6426b1c5bb3137d6f1d
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-03-03 15:26:42 -0300

    Add invoking static methods Add type cast methods parameters

---
+++ IntegerUtil.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pavel_Verkhovtsov
+ * Date: 3/3/2015
+ * Time: 19:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace OSiSP_lab1
+{
+    class IntegerUtil 
+    {
+    	public IntegerUtil(int i){}
+    	
+        public int ReturnFive(byte b){
+            return 5;
+        }
+
+        public int ReturnTwo(int i) {
+            return 2;
+        }
+
+       	public int ReturnOne(int i, byte b){
+    		return 1;
+    	}
+    	
+    	public int Sum(int i, int j){
+    		return i+j;
+    	}
+    	
+    	public static double Pow(double x, double power){
+    		return Math.Pow(x, power);
+    	}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,80 +6,97 @@ namespace OSiSP_lab1
     class Program
     {
         static void PrintAllPublicMethods(string className) {
+    		Console.WriteLine("Methods in "+className+" class:");
             Type myType = Type.GetType("OSiSP_lab1."+className, false, true);
 
 			MethodInfo[] methods = myType.GetMethods ();
 
 			for (int i=0; i<methods.Length; i++)
             {
-                Console.Write(methods[i].Name);
-                ParameterInfo[] parameters = methods[i].GetParameters();
-                Console.Write("(");
+                Console.Write(" "+i+"->"+methods[i].Name);
+                Console.WriteLine(convertParametersListToString(methods[i]));
+            }
+        }
+
+    	static string convertParametersListToString(MethodInfo method){
+    			ParameterInfo[] parameters = method.GetParameters();
+                string parametersString ="(";
                 foreach (ParameterInfo param in parameters)
                 {
-                    Console.Write(param.ParameterType.Name + " " + param.Name);
+                    parametersString+=param.ParameterType.Name + " "+param.Name+", ";
                 }
-                Console.WriteLine(")");
+                parametersString= parametersString.TrimEnd(new char[]{' ',','});
+                parametersString+=")";
+               return parametersString;
+    	}
+    	
+    	static string convertParametersListToString(ConstructorInfo constructor){
+    			ParameterInfo[] parameters = constructor.GetParameters();
+                string parametersString ="";
+                foreach (ParameterInfo param in parameters)
+                {
+                    parametersString+=param.ParameterType.Name + " "+param.Name+", ";
+                }
+               return parametersString.TrimEnd(new char[]{' ',','});
+    	}
+    	
+    	
+    	static object[] inputParameters(ParameterInfo[] parameters){
+    		object[] param = new object[parameters.Length];
+    		for (int i = 0; i < parameters.Length; i++)
+            {
+            	Console.Write("Please enter "+parameters[i].ParameterType.Name+" parameter > ");
+            	try{
+                	param[i] = Convert.ChangeType(Console.ReadLine(), Type.GetType("System."+parameters[i].ParameterType.Name, false, true));
+            	}catch(FormatException){
+            		Console.WriteLine("Incorrect parameter type");
+            		i--;
+            	}
+
             }
-        } 
+    		return param;
+    	}
 
 
         static void Main(string[] args)
         {
-            PrintAllPublicMethods("DLL");
+        	string className = "IntegerUtil";
+            PrintAllPublicMethods(className);
 
-            string methodName = Console.ReadLine();
-            MethodInfo m = checkMethodName(methodName);
-            Console.WriteLine(CallMethods(m));
+            int methodNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(CallMethods(methodNumber, className));
             Console.ReadLine();
         }
 
-
-        static MethodInfo checkMethodName(string methodName){
-            Type myType = Type.GetType("OSiSP_lab1.DLL", false, true);
-            MethodInfo[] methods = myType.GetMethods();
-
-            foreach(MethodInfo method in methods){
-                if(method.Name.Equals(methodName)){
-                    return method;
-                }
-            }
-            return null;
-        }
-
-        static object CallMethods(MethodInfo method)
+        static object CallMethods(int methodNumber, String className)
         {
-            Type myType = Type.GetType("OSiSP_lab1.DLL", false, true);
+            Type myType = Type.GetType("OSiSP_lab1."+className, false, true);
             MethodInfo[] methods = myType.GetMethods();
 
-            ParameterInfo[] parameters = method.GetParameters();
-            Console.Write("(");
-            foreach (ParameterInfo parameter in parameters)
-            {
-                Console.Write(parameter.ParameterType.Name + " " + parameter.Name);
-            }
-            Console.WriteLine(")");
+            ParameterInfo[] parameters = methods[methodNumber].GetParameters();
+            
+            Console.Write(methods[methodNumber].Name);
+            Console.WriteLine(convertParametersListToString(methods[methodNumber]));
+            
+            object[] param = inputParameters(parameters);
 
-            if (parameters.Length != 0)
-            {
-                Console.WriteLine("Please input methods parameters");
-            }
-
-            object[] param = new object[parameters.Length];
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                param[i] = (object)Console.ReadLine();
-
-              /*  Type parameterType = Type.GetType("System."+parameters[i].ParameterType.Name, false, true);
-
-                foreach (MethodInfo m in parameterType.GetMethods())
-                {
-                    Console.Write(m.Name);
-                }*/
-
-            }
-            DLL dll = new DLL();
-            return method.Invoke(dll,param);
+                    
+        	if(methods[methodNumber].IsStatic){
+        		return methods[methodNumber].Invoke(null,param);
+        	}
+        	else{
+            	ConstructorInfo conct = myType.GetConstructors()[0];
+        		string conctParam = convertParametersListToString(conct);
+        		if(!conctParam.Equals("")){
+        			Console.WriteLine("Constructor parameters: "+conctParam);
+        		}
+        	
+        		ParameterInfo[] conctParameters = conct.GetParameters();
+        		object[] conctParam1 = inputParameters(conctParameters);
+        		
+            	var dllClass = conct.Invoke(conctParam1);
+            	return methods[methodNumber].Invoke(dllClass,param);
+        	}
         }
     }
 }
