fbe2361610daf206163d0cb5b2fc026f6bc9f1ba
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-03-31 15:44:01 -0300

    Initial commit

---
+++ Container.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace OSiSP_3
+{
+	class Container
+	{
+		private object[] storage;
+		private int currentCapacity;
+		private int currentElementsCount;
+		
+		private readonly int  _initialCapacity;
+		
+		public void Reset(){
+			lock(storage){
+				currentElementsCount = 0;
+				Array.Resize<object>(ref storage,_initialCapacity);
+			}
+		}
+		
+		public int getCount(){
+			return currentElementsCount;
+		}
+		
+		public Container(int initialCapacity){
+			_initialCapacity = initialCapacity;
+			currentCapacity = initialCapacity;
+			storage = new object[initialCapacity];
+		}
+		
+		
+		
+		public void AddElement(object obj){
+			lock(storage){
+				if(currentCapacity==currentElementsCount){
+					UpdateStorageSize();
+				}
+					storage[currentElementsCount] = obj;
+					currentElementsCount++;
+			}
+		}
+		
+		public bool AddElement(object obj, int number){
+			if(number<0 || number>currentElementsCount){
+				return false;
+			}
+			if(number==currentElementsCount){
+				AddElement(obj);
+			}
+			
+			lock(storage){
+				if(currentCapacity==currentElementsCount){
+					UpdateStorageSize();
+				}
+				for(int i = currentElementsCount+1; i > number+1; i--){
+					storage[i]=storage[i-1];
+				}
+				storage[number] = obj;
+			}
+			return true;
+		}
+		
+		public object ChangeElement(object obj, int number){
+			object oldValue = null;
+			lock(storage){
+				oldValue = storage[number];
+				storage[number] = obj;
+			}
+			
+			return oldValue;
+		}
+		
+		public bool DeleteElement(object obj){
+			
+			for(int i=0; i<currentElementsCount;i++){
+				if(storage[i].Equals(obj))
+				{
+				   return DeleteElement(i);
+				 }
+			}
+			return false;
+		}
+		
+		public bool DeleteElement(int number){
+			if(number>currentElementsCount || number<0){
+				return false;
+			}
+			lock(storage){
+				for(int i = number; i<currentElementsCount-1; i++){
+					storage[i]=storage[i+1];
+				}
+			}
+			currentElementsCount--;
+			return true;
+		}
+		
+		
+		
+		
+		private void UpdateStorageSize(){
+			lock(storage){
+				Array.Resize<object>(ref storage,currentCapacity*2);
+				currentCapacity*=2;
+			}
+		}
+	}
+	
+}
---
+++ Tests/Positive/FillContainerVariousTypesTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OSiSP_3.Tests.Positive
+{
+	[TestFixture]
+	public class CreateContainerTest
+	{
+		private Container container;
+		private Random random;
+		private const string abc = "abcdefghijklmnopqrstuvwxyz";
+		
+		[Test]
+		public void FillContainerInteger()
+		{
+			container.Reset();
+			
+			for(int i=0; i<10; i++){
+				container.AddElement(random.Next());
+			}
+			
+			Assert.AreEqual(container.getCount(), 10);
+		}
+		
+		[Test]
+		public void FillContainerDouble()
+		{
+			container.Reset();
+			
+			for(int i=0; i<10; i++){
+				container.AddElement(random.NextDouble());
+			}
+			
+			Assert.AreEqual(container.getCount(), 10); 
+		}
+		
+		
+		
+		[Test]
+		public void FillContainerString()
+		{
+			container.Reset();
+			
+			for(int i=0; i<10; i++){
+				string str = "";
+				for(int j=0; j < random.Next(20, 100); j++){
+					str+=abc[random.Next()%abc.Length];
+				}
+				container.AddElement(str);
+			}
+			
+			Assert.AreEqual(container.getCount(), 10); 
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			container = new Container(100);
+			random = new Random();
+		}
+	}
+}
+
