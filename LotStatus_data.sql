d47d89aaf72100aa4084423561bfe79ae1a2f5fb
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-04-17 14:35:27 +0300

    DB MySql auction

---
+++ LotStatus_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lotsstatus` (`lot_status_id`,`status`) VALUES (NULL,'Выставлен на продажу');
+INSERT INTO `lotsstatus` (`lot_status_id`,`status`) VALUES (NULL,'Участвует в аукционе');
+INSERT INTO `lotsstatus` (`lot_status_id`,`status`) VALUES (NULL,'Продан');
