268f7852dd23247a43b7d9850553c8f38aab8e2e
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-05-21 22:36:22 +0300

    Start implement resize WPF

--- WPF/Window1.xaml.cs
+++ WPF/Window1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
 using PhotoEditor;
@@ -8,14 +9,15 @@ using PhotoEditor;
 
 namespace WPF
 {
-	public partial class Window1 : Window
+	public partial class MainWindow : Window
 	{
 		private BitmapImage bitmap;
 		private BitmapImage originalBitmap;
 
-		bool isFromFile;
+		private int zoomCounter;
+		private bool isFromFile;
 		
-		public Window1()
+		public MainWindow()
 		{
 			InitializeComponent();
 		}
@@ -62,6 +64,32 @@ namespace WPF
 					blueColorTrack.Value);
 				mainPictureBox.Source = bitmap;
 			}
+		}
+		void resize_plus_Click(object sender, RoutedEventArgs e)
+		{
+			if (zoomCounter < 5) {
+				/*rigthDown.X = rigthDown.X + 5 * rigthDown.X / 100;
+				rigthDown.Y = rigthDown.Y + 5 * rigthDown.Y / 100;
+				leftUp.X = leftUp.X + 5 * leftUp.X / 100;
+				leftUp.Y = leftUp.Y + 5 * leftUp.Y / 100;*/
+				
+				BitmapImage newSizeImage = (BitmapImage)(new TransformedBitmap(originalBitmap, new ScaleTransform( (bitmap.Width + 5 * (bitmap.Width / 100)) / bitmap.PixelWidth,  (bitmap.Height + 5 * (bitmap.Height / 100)) / bitmap.PixelHeight)).Source);
+				
+				bitmap = PhotoEditHelper.AdjustImage(newSizeImage ,  brightnessTrack.Value, contrastTrack.Value, redColorTrack.Value, greenColorTrack.Value,
+					blueColorTrack.Value);
+				mainPictureBox.Width = bitmap.Width;
+				mainPictureBox.Height = bitmap.Height;
+				mainPictureBox.Source = bitmap;
+				
+				/*Graphics g = Graphics.FromImage(this.mainPictureBox.Image);
+				g.DrawRectangle(new Pen(Color.Black), leftUp.X, leftUp.Y, rigthDown.X - leftUp.X, rigthDown.Y - leftUp.Y);*/
+				
+				zoomCounter++;
+			}
+		}
+		void resize_minus_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
