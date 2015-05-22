c6935195966400fa7733b9cffb2ef21bea7f4d68
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-05-22 21:37:01 +0300

    Resize and selection

--- WPF/PhotoEditHelper.cs
+++ WPF/PhotoEditHelper.cs
@@ -9,9 +9,10 @@ namespace PhotoEditor
 {
 	public static class PhotoEditHelper
 	{
-		private static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap){
+		private static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+		{
 			MemoryStream stream = new MemoryStream();
-			bitmap.Save(stream, ImageFormat.Jpeg);
+			bitmap.Save(stream, ImageFormat.Png);
 			stream.Position = 0;
 			
 			BitmapImage result = new BitmapImage();
@@ -24,18 +25,18 @@ namespace PhotoEditor
 			return result;
 		}
 		
-		private static Bitmap ConvertBitmapImageToBitmap(BitmapImage bitmapImage){
-			using(var outStream = new MemoryStream()){
-				BitmapEncoder enc = new JpegBitmapEncoder();
+		private static Bitmap ConvertBitmapImageToBitmap(BitmapImage bitmapImage)
+		{
+			using (var outStream = new MemoryStream()) {
+				BitmapEncoder enc = new PngBitmapEncoder();
 				enc.Frames.Add(BitmapFrame.Create(bitmapImage));
 				enc.Save(outStream);
 				var bitmap = new Bitmap(outStream);
 				return new Bitmap(bitmap);
 			}
-			
 		}
 		
-		public static BitmapImage AdjustImage(BitmapImage sourceBitmap, double brightness, double contrast, double redColor, double greenColor, double blueColor)
+		public static BitmapImage AdjustImage(BitmapImage sourceBitmap, double brightness, double contrast, double redColor, double greenColor, double blueColor, double startX, double startY, double h, double w)
 		{
 			Bitmap bitmap = ConvertBitmapImageToBitmap(sourceBitmap);
 			
@@ -44,31 +45,36 @@ namespace PhotoEditor
 			greenColor = greenColor / 255 + 1;
 			blueColor = blueColor / 255 + 1;
 			float[][] matrix = {
-				new[] {(float)redColor, 0, 0, 0, 0},
-				new[] {0, (float)greenColor, 0, 0, 0},
-				new[] {0, 0, (float)blueColor, 0, 0},
-                new[] {0, 0, 0, 1f, 0},
-                new[] {(float)brightness, (float)brightness, (float)brightness, 0, 1}
-            };
+				new[] { (float)redColor, 0, 0, 0, 0 },
+				new[] { 0, (float)greenColor, 0, 0, 0 },
+				new[] { 0, 0, (float)blueColor, 0, 0 },
+				new[] { 0, 0, 0, 1f, 0 },
+				new[] {
+					(float)brightness,
+					(float)brightness,
+					(float)brightness,
+					0,
+					1
+				}
+			};
 
 
 			ImageAttributes attributes = new ImageAttributes();
-            attributes.SetColorMatrix(new ColorMatrix(matrix), ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-            attributes.SetGamma((float)contrast, ColorAdjustType.Bitmap);
+			attributes.SetColorMatrix(new ColorMatrix(matrix), ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+			attributes.SetGamma((float)contrast, ColorAdjustType.Bitmap);
 
-            var newImage = new Bitmap(bitmap.Width, bitmap.Height);
-            using (var graphics = Graphics.FromImage(newImage))
-                graphics.DrawImage(bitmap, new System.Drawing.Rectangle(0, 0, newImage.Width, newImage.Height), 0, 0, newImage.Width,
-                    newImage.Height,  GraphicsUnit.Pixel, attributes);
-            return ConvertBitmapToBitmapImage(newImage);
-        }	
+			var newImage = new Bitmap(bitmap.Width, bitmap.Height);
+			using (var graphics = Graphics.FromImage(newImage)) {
+				graphics.DrawImage(bitmap, 0, 0, newImage.Width, newImage.Height);
+				graphics.DrawImage(bitmap, new System.Drawing.Rectangle((int)startX, (int)startY, (int)w, (int)h), (int)startX, (int)startY, (int)w, (int)h,
+					GraphicsUnit.Pixel, attributes);
+			}
+			return ConvertBitmapToBitmapImage(newImage);
+		}
 		
-		private static void CheckValue(ref double value)
+		public static BitmapImage Resize(BitmapImage source, int width, int height)
 		{
-	    	if (value > 255) 
-	    	    value = 255;
-	        else if (value < -255) 
-	    	    value = 255;
+			return ConvertBitmapToBitmapImage(new Bitmap(ConvertBitmapImageToBitmap(source), width, height));
 		}
 	}
 }
--- WPF/Window1.xaml.cs
+++ WPF/Window1.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using Microsoft.Win32;
 using PhotoEditor;
 
@@ -13,9 +16,19 @@ namespace WPF
 	{
 		private BitmapImage bitmap;
 		private BitmapImage originalBitmap;
-
+		
 		private int zoomCounter;
-		private bool isFromFile;
+		private bool isImageOpen;
+		
+			
+		private Point startPoint;
+		private Rectangle rect;
+		
+		
+		private double x;
+		private double y;
+		private double w;
+		private double h;
 		
 		public MainWindow()
 		{
@@ -32,14 +45,26 @@ namespace WPF
 				originalBitmap = bitmap;
 				mainPictureBox.Height = bitmap.Height;
 				mainPictureBox.Width = bitmap.Width;
+					
+				rect = new Rectangle {
+				Stroke = Brushes.LightBlue,
+				StrokeThickness = 0
+				};
+				
+				x = 0;
+				y = 0;
+				w = bitmap.Width;
+				h = bitmap.Height;
+				
 				mainPictureBox.Source = bitmap;
-				isFromFile = true;
+				isImageOpen = true;
+				zoomCounter = 0;
 			}
 		}
 		
 		void saveFile_Click(object sender, RoutedEventArgs e)
 		{
-			if (isFromFile) {
+			if (isImageOpen) {
 				SaveFileDialog saveFileDialog = new SaveFileDialog();
 				if (saveFileDialog.ShowDialog() == true) {
 					JpegBitmapEncoder jpegBitmapEncoder = new JpegBitmapEncoder();
@@ -47,7 +72,7 @@ namespace WPF
 					jpegBitmapEncoder.Frames.Add(BitmapFrame.Create(bitmap));
 					string fileName = saveFileDialog.FileName;
 					
-					if (File.Exists(fileName)){
+					if (File.Exists(fileName)) {
 						File.Delete(fileName);
 					}
 					
@@ -58,38 +83,95 @@ namespace WPF
 			}
 		}
 		
-		private void changeImage(object sender, RoutedPropertyChangedEventArgs<double> e){
-			if (isFromFile) {
+		private void changeImage(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			if (isImageOpen) {
 				bitmap = PhotoEditHelper.AdjustImage(originalBitmap, brightnessTrack.Value, contrastTrack.Value, redColorTrack.Value, greenColorTrack.Value,
-					blueColorTrack.Value);
+					blueColorTrack.Value, x, y, h, w);
 				mainPictureBox.Source = bitmap;
 			}
 		}
 		void resize_plus_Click(object sender, RoutedEventArgs e)
 		{
-			if (zoomCounter < 5) {
-				/*rigthDown.X = rigthDown.X + 5 * rigthDown.X / 100;
-				rigthDown.Y = rigthDown.Y + 5 * rigthDown.Y / 100;
-				leftUp.X = leftUp.X + 5 * leftUp.X / 100;
-				leftUp.Y = leftUp.Y + 5 * leftUp.Y / 100;*/
+			if (zoomCounter < 5 && isImageOpen) {
+				w = rect.Width + 5 * rect.Width / 100;
+				rect.Width = w;
+				h = rect.Height + 5 * rect.Height / 100;
+				rect.Height = h;
+				x = x + 5 * x / 100;
+				Canvas.SetLeft(rect, x);
+				y = y + 5* y / 100;
+				Canvas.SetTop(rect, y);
+
+				BitmapImage newSizeImage = PhotoEditHelper.Resize(originalBitmap, (int)(bitmap.Width + 5 * (bitmap.Width / 100)), (int)(bitmap.Height + 5 * (bitmap.Height / 100)));
 				
-				BitmapImage newSizeImage = (BitmapImage)(new TransformedBitmap(originalBitmap, new ScaleTransform( (bitmap.Width + 5 * (bitmap.Width / 100)) / bitmap.PixelWidth,  (bitmap.Height + 5 * (bitmap.Height / 100)) / bitmap.PixelHeight)).Source);
-				
-				bitmap = PhotoEditHelper.AdjustImage(newSizeImage ,  brightnessTrack.Value, contrastTrack.Value, redColorTrack.Value, greenColorTrack.Value,
-					blueColorTrack.Value);
+				bitmap = PhotoEditHelper.AdjustImage(newSizeImage, brightnessTrack.Value, contrastTrack.Value, redColorTrack.Value, greenColorTrack.Value,
+					blueColorTrack.Value, x, y, h, w);
 				mainPictureBox.Width = bitmap.Width;
 				mainPictureBox.Height = bitmap.Height;
 				mainPictureBox.Source = bitmap;
-				
-				/*Graphics g = Graphics.FromImage(this.mainPictureBox.Image);
-				g.DrawRectangle(new Pen(Color.Black), leftUp.X, leftUp.Y, rigthDown.X - leftUp.X, rigthDown.Y - leftUp.Y);*/
 				
 				zoomCounter++;
 			}
 		}
 		void resize_minus_Click(object sender, RoutedEventArgs e)
 		{
-			throw new NotImplementedException();
+			if (zoomCounter > -5 && isImageOpen) {
+				w = rect.Width - 5 * rect.Width / 100;
+				rect.Width = w;
+				h = rect.Height - 5 * rect.Height / 100;
+				rect.Height = h;
+				x = x - 5 * x / 100;
+				Canvas.SetLeft(rect, x);
+				y = y - 5* y / 100;
+				Canvas.SetTop(rect, y);
+				
+				BitmapImage newSizeImage = PhotoEditHelper.Resize(originalBitmap, (int)(bitmap.Width - 5 * (bitmap.Width / 100)), (int)(bitmap.Height - 5 * (bitmap.Height / 100)));
+				
+				bitmap = PhotoEditHelper.AdjustImage(newSizeImage, brightnessTrack.Value, contrastTrack.Value, redColorTrack.Value, greenColorTrack.Value,
+					blueColorTrack.Value, x, y, h, w);
+				mainPictureBox.Width = bitmap.Width;
+				mainPictureBox.Height = bitmap.Height;
+				mainPictureBox.Source = bitmap;
+				
+				zoomCounter--;
+			}
+		}
+
+		void mainPictureBox_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Released || rect == null)
+				return;
+
+			var pos = e.GetPosition(canvas);
+
+			x = Math.Min(pos.X, startPoint.X);
+			y = Math.Min(pos.Y, startPoint.Y);
+
+			w = Math.Max(pos.X, startPoint.X) - x;
+			h = Math.Max(pos.Y, startPoint.Y) - y;
+
+			rect.Width = w;
+			rect.Height = h;
+
+			Canvas.SetLeft(rect, x);
+			Canvas.SetTop(rect, y);
+			
+			changeImage(sender , null);
+		}
+
+		void mainPictureBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			canvas.Children.Remove(rect);
+			startPoint = e.GetPosition(canvas);
+
+			rect = new Rectangle {
+				Stroke = Brushes.LightBlue,
+				StrokeThickness = 2
+			};
+			Canvas.SetLeft(rect, startPoint.X);
+			Canvas.SetTop(rect, startPoint.Y);
+			canvas.Children.Add(rect);
 		}
 	}
 }
