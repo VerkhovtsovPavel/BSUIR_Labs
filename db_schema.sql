4a4aae8c00867132f76bd03ec05365c17de8a279
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-09-02 22:23:02 +0300

    Add model

--- db_schema.sql
+++ db_schema.sql
@@ -1,160 +1,153 @@
--- ---
--- Globals
--- ---
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+CREATE SCHEMA IF NOT EXISTS `bank_users` DEFAULT CHARACTER SET utf8 ;
+USE `bank_users` ;
 
--- ---
--- Table 'User'
--- 
--- ---
+-- -----------------------------------------------------
+-- Table `bank_users`.`address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`address` ;
 
-DROP TABLE IF EXISTS `User`;
-		
-CREATE TABLE `User` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
-  `FirstName` VARCHAR(255) NOT NULL,
-  `LastName` VARCHAR(255) NOT NULL,
-  `MidleName` VARCHAR(255) NOT NULL,
-  `Birthday` DATE NOT NULL DEFAULT 'NULL',
-  `Sex_id` TINYINT NOT NULL DEFAULT NULL,
-  `Passport_id` INTEGER NOT NULL,
-  `Address_id` INTEGER NOT NULL,
-  `MobilePhone` VARCHAR(255) NULL,
-  `HomePhone` VARCHAR NULL,
-  `E-mail` VARCHAR(255) NULL,
-  `FamilyStatus` INTEGER NOT NULL,
-  `Nationality_id` INTEGER NOT NULL,
-  `Disability_id` INTEGER NOT NULL,
-  `Pensioner` TINYINT NOT NULL,
-  `MonthProfit` INTEGER NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE  TABLE IF NOT EXISTS `bank_users`.`address` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `RealCity` VARCHAR(255) NOT NULL ,
+  `RealStreet` VARCHAR(255) NOT NULL ,
+  `OfficialStreet` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- ---
--- Table 'Sexs'
--- 
--- ---
 
-DROP TABLE IF EXISTS `Sexs`;
-		
-CREATE TABLE `Sexs` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Sex` VARCHAR(10) NOT NULL DEFAULT 'NULL',
-  PRIMARY KEY (`id`)
-);
+-- -----------------------------------------------------
+-- Table `bank_users`.`disabilitys`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`disabilitys` ;
 
--- ---
--- Table 'PassportInfo'
--- 
--- ---
+CREATE  TABLE IF NOT EXISTS `bank_users`.`disabilitys` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Disability` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-DROP TABLE IF EXISTS `PassportInfo`;
-		
-CREATE TABLE `PassportInfo` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Serios` CHAR(2) NOT NULL,
-  `Number` VARCHAR(15) NOT NULL,
-  `WhoGives` VARCHAR(255) NOT NULL,
-  `DateGives` DATE NOT NULL,
-  `IndifyNumber` VARCHAR(255) NOT NULL,
-  `BornPlace` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
 
--- ---
--- Table 'Address'
--- 
--- ---
+-- -----------------------------------------------------
+-- Table `bank_users`.`familystatuses`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`familystatuses` ;
 
-DROP TABLE IF EXISTS `Address`;
-		
-CREATE TABLE `Address` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `RealCity` VARCHAR NOT NULL,
-  `RealStreet` VARCHAR(255) NOT NULL,
-  `OfficialStreet` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE  TABLE IF NOT EXISTS `bank_users`.`familystatuses` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Status` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- ---
--- Table 'FamilyStatuses'
--- 
--- ---
 
-DROP TABLE IF EXISTS `FamilyStatuses`;
-		
-CREATE TABLE `FamilyStatuses` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Status` VARCHAR NOT NULL,
-  PRIMARY KEY (`id`)
-);
+-- -----------------------------------------------------
+-- Table `bank_users`.`nationalitys`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`nationalitys` ;
 
--- ---
--- Table 'Nationalitys'
--- 
--- ---
+CREATE  TABLE IF NOT EXISTS `bank_users`.`nationalitys` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Nationality` VARCHAR(255) NOT NULL DEFAULT 'NULL' ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-DROP TABLE IF EXISTS `Nationalitys`;
-		
-CREATE TABLE `Nationalitys` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Nationality` VARCHAR(255) NOT NULL DEFAULT 'NULL',
-  PRIMARY KEY (`id`)
-);
 
--- ---
--- Table 'Disabilitys'
--- 
--- ---
+-- -----------------------------------------------------
+-- Table `bank_users`.`passportinfo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`passportinfo` ;
 
-DROP TABLE IF EXISTS `Disabilitys`;
-		
-CREATE TABLE `Disabilitys` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Disability` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE  TABLE IF NOT EXISTS `bank_users`.`passportinfo` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Serios` CHAR(2) NOT NULL ,
+  `Number` VARCHAR(15) NOT NULL ,
+  `WhoGives` VARCHAR(255) NOT NULL ,
+  `DateGives` DATE NOT NULL ,
+  `IndifyNumber` VARCHAR(255) NOT NULL ,
+  `BornPlace` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- ---
--- Foreign Keys 
--- ---
 
-ALTER TABLE `User` ADD FOREIGN KEY (Sex_id) REFERENCES `Sexs` (`id`);
-ALTER TABLE `User` ADD FOREIGN KEY (Passport_id) REFERENCES `PassportInfo` (`id`);
-ALTER TABLE `User` ADD FOREIGN KEY (Address_id) REFERENCES `Address` (`id`);
-ALTER TABLE `User` ADD FOREIGN KEY (FamilyStatus) REFERENCES `FamilyStatuses` (`id`);
-ALTER TABLE `User` ADD FOREIGN KEY (Nationality_id) REFERENCES `Nationalitys` (`id`);
-ALTER TABLE `User` ADD FOREIGN KEY (Disability_id) REFERENCES `Disabilitys` (`id`);
+-- -----------------------------------------------------
+-- Table `bank_users`.`sexs`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`sexs` ;
 
--- ---
--- Table Properties
--- ---
+CREATE  TABLE IF NOT EXISTS `bank_users`.`sexs` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `Sex` VARCHAR(10) NOT NULL DEFAULT 'NULL' ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
--- ALTER TABLE `User` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Sexs` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `PassportInfo` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Address` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `FamilyStatuses` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Nationalitys` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Disabilitys` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- ---
--- Test Data
--- ---
+-- -----------------------------------------------------
+-- Table `bank_users`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bank_users`.`user` ;
 
--- INSERT INTO `User` (`id`,`FirstName`,`LastName`,`MidleName`,`Birthday`,`Sex_id`,`Passport_id`,`Address_id`,`MobilePhone`,`HomePhone`,`E-mail`,`FamilyStatus`,`Nationality_id`,`Disability_id`,`Pensioner`,`MonthProfit`) VALUES
--- ('','','','','','','','','','','','','','','','');
--- INSERT INTO `Sexs` (`id`,`Sex`) VALUES
--- ('','');
--- INSERT INTO `PassportInfo` (`id`,`Serios`,`Number`,`WhoGives`,`DateGives`,`IndifyNumber`,`BornPlace`) VALUES
--- ('','','','','','','');
--- INSERT INTO `Address` (`id`,`RealCity`,`RealStreet`,`OfficialStreet`) VALUES
--- ('','','','');
--- INSERT INTO `FamilyStatuses` (`id`,`Status`) VALUES
--- ('','');
--- INSERT INTO `Nationalitys` (`id`,`Nationality`) VALUES
--- ('','');
--- INSERT INTO `Disabilitys` (`id`,`Disability`) VALUES
--- ('','');
+CREATE  TABLE IF NOT EXISTS `bank_users`.`user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `FirstName` VARCHAR(255) NOT NULL ,
+  `LastName` VARCHAR(255) NOT NULL ,
+  `MidleName` VARCHAR(255) NOT NULL ,
+  `Birthday` DATE NOT NULL ,
+  `Sex_id` INT NOT NULL ,
+  `Passport_id` INT(11) NOT NULL ,
+  `Address_id` INT(11) NOT NULL ,
+  `MobilePhone` VARCHAR(255) NULL DEFAULT NULL ,
+  `HomePhone` VARCHAR(255) NULL DEFAULT NULL ,
+  `E-mail` VARCHAR(255) NULL DEFAULT NULL ,
+  `FamilyStatus` INT(11) NOT NULL ,
+  `Nationality_id` INT(11) NOT NULL ,
+  `Disability_id` INT(11) NOT NULL ,
+  `Pensioner` TINYINT(4) NOT NULL ,
+  `MonthProfit` INT(11) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `Address_id` (`Address_id` ASC) ,
+  INDEX `FamilyStatus` (`FamilyStatus` ASC) ,
+  INDEX `Nationality_id` (`Nationality_id` ASC) ,
+  INDEX `Disability_id` (`Disability_id` ASC) ,
+  INDEX `Sex_id` (`Sex_id` ASC) ,
+  INDEX `user_sex` (`Sex_id` ASC) ,
+  CONSTRAINT `user_ibfk_1`
+    FOREIGN KEY (`Passport_id` )
+    REFERENCES `bank_users`.`passportinfo` (`id` )
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `user_ibfk_2`
+    FOREIGN KEY (`Address_id` )
+    REFERENCES `bank_users`.`address` (`id` ),
+  CONSTRAINT `user_ibfk_3`
+    FOREIGN KEY (`FamilyStatus` )
+    REFERENCES `bank_users`.`familystatuses` (`id` ),
+  CONSTRAINT `user_ibfk_4`
+    FOREIGN KEY (`Nationality_id` )
+    REFERENCES `bank_users`.`nationalitys` (`id` ),
+  CONSTRAINT `user_ibfk_5`
+    FOREIGN KEY (`Disability_id` )
+    REFERENCES `bank_users`.`disabilitys` (`id` )
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `user_sex`
+    FOREIGN KEY (`Sex_id` )
+    REFERENCES `bank_users`.`sexs` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
