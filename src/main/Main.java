06dcfe6e87187c47844899f1d85fbeed1d5ca318
Pavel Verkhovtsov <verchpasha@gmail.com>
2015-10-02 11:43:08 +0300

    Final version

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,30 @@
 package main;
 
+import java.util.Scanner;
+
 import queuingSystem.clientGenerator.ClientGenerator;
 import queuingSystem.firstStepOfProcessing.FirstStepOfProcessing;
 import queuingSystem.queue.QSQueue;
 import queuingSystem.secondStepOfProcessing.SecondStepOfProcessing;
 
 public class Main {
-    private static final double prodationOfGenerator = 0.75;
-    private static final double prodationOfFirstStepOfProcessing = 0.7;
-    private static final double prodationOfSecondStepOfProcessing = 0.65;
+	private static Scanner in = new Scanner(System.in);
+    private static double prodationOfGenerator = 0.75;
+    private static double prodationOfFirstStepOfProcessing = 0.7;
+    private static double prodationOfSecondStepOfProcessing = 0.65;
     
-    private static final int countOfTacts = 1000000;
+    private static int countOfTacts = 1000000000;
     
     public static void main(String[] args) {
+    	System.out.print("Please input count of tacts > ");
+    	countOfTacts = in.nextInt();
+
+    	System.out.print("Please input elements probations > ");
+    	prodationOfGenerator = in.nextDouble();
+    	prodationOfFirstStepOfProcessing = in.nextDouble();
+    	prodationOfSecondStepOfProcessing = in.nextDouble();
+    	
+    	
        SecondStepOfProcessing secondStepOfProcessing = new SecondStepOfProcessing(prodationOfSecondStepOfProcessing);
        QSQueue cmoQueue = new QSQueue(secondStepOfProcessing);
        FirstStepOfProcessing firstStepOfProcessing = new FirstStepOfProcessing(prodationOfFirstStepOfProcessing, cmoQueue);
@@ -26,11 +38,9 @@ public class Main {
            
            clientGenerator.pushClient(true);
        }
-       
+       System.out.println("Count of tacts > "+ countOfTacts);
        System.out.println("Absolutely bandwidth> "+(float)secondStepOfProcessing.processedTask / countOfTacts);
        System.out.println("Average queue length> "+(float)cmoQueue.totalLengthQuequ/countOfTacts);
        System.out.println("Average time in queue> "+(float)cmoQueue.totalTimeInQuequ/secondStepOfProcessing.processedTask);
     }
-
-
 }
