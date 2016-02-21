bd606142c1553f06f274f54d1dcc06eb6013c57c
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-02-21 21:14:44 +0300

    Add search method

--- src/by/verkpavel/iofs/btree/BTree.scala
+++ src/by/verkpavel/iofs/btree/BTree.scala
@@ -1,13 +1,21 @@
 package by.verkpavel.iofs.btree
 
 
-class BTree[T] {
+class BTree[T <: Ordered[T]](nodeSize : Int, listSize : Int) {
 
+  val root = Node(List[NodeItem](), List[Node]())
+
+  def ? (item : T, node : BTreeItem = root) : Boolean = {
+    node match {
+      case Node(items, links) => val link = items.count((i) => i.value < item);  ?(item, links.apply(link))
+      case Leaf(values) => values.contains(item)
+    }
+  }
 
   sealed class BTreeItem
   case class Leaf(val values : List[T]) extends BTreeItem
-  case class NodeItem(val child : BTreeItem, val value : T) extends BTreeItem
-  case class Node(val items : List[NodeItem]) extends BTreeItem
+  case class NodeItem(val value : T) extends BTreeItem
+  case class Node(val items : List[NodeItem], val links : List[BTreeItem]) extends BTreeItem
 }
 
 
