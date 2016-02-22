87c6eee80c64a2fc07dae6418d441a951fd89911
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-02-22 14:02:05 +0300

    Add partial add method implementation

--- src/by/verkpavel/iofs/btree/BTree.scala
+++ src/by/verkpavel/iofs/btree/BTree.scala
@@ -1,21 +1,39 @@
 package by.verkpavel.iofs.btree
 
-
 class BTree[T <: Ordered[T]](nodeSize : Int, listSize : Int) {
 
-  val root = Node(List[NodeItem](), List[Node]())
+  val root = Node(List[T](), List[Node](), null)
 
-  def ? (item : T, node : BTreeItem = root) : Boolean = {
+  private def search (item : T, node : BTreeItem = root) : (Boolean, Leaf) = {
     node match {
-      case Node(items, links) => val link = items.count((i) => i.value < item);  ?(item, links.apply(link))
-      case Leaf(values) => values.contains(item)
+      case Node(items, links, _) => val link = items.count((i) => i < item);  search(item, links.apply(link))
+      case leaf @ Leaf(values, _) => (values.contains(item), leaf)
     }
   }
 
+  def ? (item : T) = search(item)._1
+
+  def + (item : T) = {
+    val leaf = search(item)._2
+
+    if(leaf.values.size<listSize)
+    {
+      leaf.values = leaf.values :+ item
+      leaf.values.sortWith(_ < _)
+    }
+    else if(leaf.parent.items.size < nodeSize){
+      val parentNode = leaf.parent
+      val newLeaf = Leaf(leaf.values.slice(listSize/2, listSize), leaf.parent)
+      leaf.values = leaf.values.slice(0, listSize/2)
+      parentNode.items = parentNode.items :+ newLeaf.values.head
+      parentNode.links =  parentNode.links :+ newLeaf
+    }else{}
+
+  }
+
   sealed class BTreeItem
-  case class Leaf(val values : List[T]) extends BTreeItem
-  case class NodeItem(val value : T) extends BTreeItem
-  case class Node(val items : List[NodeItem], val links : List[BTreeItem]) extends BTreeItem
+  case class Leaf(var values : List[T], parent: Node) extends BTreeItem
+  case class Node(var items : List[T],  var links : List[BTreeItem], parent : Node) extends BTreeItem
 }
 
 
--- src/by/verkpavel/iofs/main/Main.scala
+++ src/by/verkpavel/iofs/main/Main.scala
@@ -1,34 +1,30 @@
 package by.verkpavel.iofs.main
 
-import by.verkpavel.iofs.btree.{BTreeAlg, BTree}
 import by.verkpavel.iofs.hash.HashTable
 import scala.io.Source
 import by.verkpavel.iofs.utils.Chronometry.time
 
-import scala.util.Random
-
 object Main extends App{
-
   bTreeTest()
   hashTableTest()
 
 
 def bTreeTest() = {
   println("----------------------")
-  println("BTreeAlg")
+  println("BTree")
   println("----------------------")
-  val tree = new BTreeAlg[String,String]()
+  val tree = new by.verkpavel.iofs.btree.nm.BTree(5)
 
   println("Add elements")
-  time(Source.fromFile("res/test1000 000.txt").mkString.split("""\s+""").foreach((x:String)=>tree.put(x,x)))
+  time(Source.fromFile("res/test1000 000.txt").mkString.split("""\s+""").foreach((x:String)=>tree.add(x)))
 
   println("Find elements")
-  time(tree.get("02iGTy7RZg"))
-  time(tree.get("dwsdsdsf"))
-  time(tree.get("CK6p08ZaAP"))
+  time(tree.search("02iGTy7RZg"))
+  time(tree.search("dwsdsdsf"))
+  time(tree.search("CK6p08ZaAP"))
 
   println("Delete element")
-  //time(tree.delete("CK6p08ZaAP"))
+  time(tree.remove("CK6p08ZaAP"))
 }
 
 
