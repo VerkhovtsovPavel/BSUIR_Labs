fedc80bb413cc569455b7ffb1fde78ba132f960a
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-03-11 22:41:53 +0300

    Rework printing grammar methods and add small magic

--- src/Main.scala
+++ src/Main.scala
@@ -6,12 +6,8 @@ object Main extends App{
   println("--------------------------Source words---------------------------")
   println(Source.fromFile("res/base.txt").mkString)
   println("--------------------------Grammar---------------------------")
-  println(grammarBuilder.getGrammar())
+  println(grammarBuilder.getGrammar)
   println("--------------------------Generated words---------------------------")
   for(_ <- 0 until 3)
     print(grammarBuilder.getWord() + " ")
-
-  //println(NonTerminate(mutable.Map("a"-> NonTerminate(mutable.Map("b" -> Terminate("b"))), "b" -> Terminate("b"))) equals NonTerminate(mutable.Map("a"-> NonTerminate(mutable.Map("b" -> Terminate("b"))), "b" -> Terminate("b"))))
-
-  //println(('a','b') equals ('a','b'))
 }
