11ec71c3e70d4c170bf421612e92ea456571c86a
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-10-27 00:47:46 +0300

    Minor ws changes

--- src/chat/model/websocket.clj
+++ src/chat/model/websocket.clj
@@ -9,11 +9,27 @@
     (on-close channel (fn [status]
                         (remove-watch bindpoint channel)))
     (on-receive channel (fn [message]
-                          (reset! bindpoint message)))
+                          (send! channel (response message channel))))
+
     (add-watch bindpoint channel
                (fn [_channel _atom _old_json json]
                  (send! channel json)))))
 
 
+(defn- response
+  [message channel]
+  (let[json json/parse-string message]
+    (json/generate-string
+      {:method (json "method")
+      :result (perform-ws-action json channel)}))
+  )
 
 
+(defmulti perform-ws-action (fn [message] (:method message)))
+
+(defmethod perform-ws-action :connect [message channel] (str "Young " (:name person)))
+(defmethod perform-ws-action :disconnect [message channel] (:name person))
+(defmethod perform-ws-action :new_message [message channel] (str "Old " (:name person)))
+
+
+;reset! bindpoint message
