b0fb9c6590b955b9986060a5bd8b92cd32f388f4
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-10-28 14:24:54 +0300

    Add multi bindpoint

--- src/chat/model/websocket.clj
+++ src/chat/model/websocket.clj
@@ -2,37 +2,57 @@
   (:use org.httpkit.server)
   (:require [cheshire.core :as jsonprs]))
 
-(defn- create_room_bindpoint [room]
-  (dosync
-    alter bindpoint assoc {room (atom nil)}))
 
-(def bindpoint ref{"global" (atom nil)})
+(def bindpoints (atom {"global" (atom nil)}))
+
+(defn- extractServerWSPort [channel]
+  (.substring (str channel) (+ (.lastIndexOf (str channel) ":") 1)))
+
+(defn- create_room_bindpoint [room]
+  (swap! bindpoints (fn [current_state] (assoc current_state {room (atom nil)}))))
+
+(defn- add-subscriper [bindpoint channel]
+  (add-watch bindpoint channel
+               (fn [_channel _atom _old_json json]
+                 (send! channel json))))
+
+(defn- beatify-message [message channel]
+  (let [datetime (java.util.Date.)
+        ctime (.format (java.text.SimpleDateFormat. "HH/mm/ss") datetime)]
+          (update message "text" (fn [old](str (extractServerWSPort channel) ": " (message "text") " /\\ " ctime )))))
 
 (defmulti perform-ws-action (fn [message channel] (message "method")))
 
 (defmethod perform-ws-action "connect"
  [message channel]
   (let [room (message "room")
-        room_bindpoint (bindpoint room)]
-        (if(!room_bindpoint)
+        room_bindpoint (@bindpoints room)]
+        (if room_bindpoint
+          (add-subscriper room_bindpoint channel)
           (do
             (create_room_bindpoint room)
-            (add-subscriper (bindpoint room) channel))
-         (add-subscriper room_bindpoint channel))
-
+            (add-subscriper (@bindpoints room) channel))
+         )
   )
+  (str "Ok")
 )
 
-(defmethod perform-ws-action "disconnect" [message channel] (str "Leave " (:room message)))
-(defmethod perform-ws-action "message" [message channel] (reset! bindpoint (jsonprs/generate-string message))(message "text"))
+(defmethod perform-ws-action "disconnect" [message channel] 
+  (let [room (message "room")
+        room_bindpoint (@bindpoints room)]
+          (remove-watch room_bindpoint channel)))
 
-(defn- injectServerWSPort [channel]
-  (.substring (str channel) (+ (.lastIndexOf (str channel) ":") 1))
-)
+(defmethod perform-ws-action "message" [message channel] 
+  (let [room (message "room")
+        bus  (@bindpoints room)
+       jsmessage (jsonprs/generate-string (beatify-message message channel))]
+       (println (str "Pushinsh to " room " message " jsmessage)) 
+       (reset! bus jsmessage))
+  (message "text"))
 
 (defn getResponse
   [message channel]
-  (println (str "Request from " (injectServerWSPort channel) " with " message))
+  (println (str "Request from " (extractServerWSPort channel) " with " message))
   (let[json (jsonprs/parse-string message)]
     (jsonprs/generate-string
       {:method (json "method")
@@ -42,12 +62,9 @@
 (defn handler [request]
   (with-channel request channel
     (on-close channel (fn [status]
-                        (remove-watch bindpoint channel)))
+                        (doseq [[k v] @bindpoints] (remove-watch v channel))))
     (on-receive channel (fn [message]
                           (let [responce (getResponse message channel)]
-                              (println (str "Responce for " (injectServerWSPort channel) " with " responce))    
-                              (send! channel responce))))
+                              (println (str "Responce for " (extractServerWSPort channel) " with " responce))
+                              (send! channel responce))))))
 
-    (add-watch bindpoint channel
-               (fn [_channel _atom _old_json json]
-                 (send! channel json)))))
