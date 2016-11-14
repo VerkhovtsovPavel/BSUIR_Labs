366c8883dd9c961f1cea5c4363b838bd9333df57
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-11-14 18:01:16 +0300

    Add resized sample set

--- src/by/bsuir/verpav/misoi/neuronNetwork/NeuronNetworkUtils.scala
+++ src/by/bsuir/verpav/misoi/neuronNetwork/NeuronNetworkUtils.scala
@@ -1,5 +1,7 @@
 package by.bsuir.verpav.misoi.neuronNetwork
 
+import scala.util.Random
+
 /**
   * Created by verkpavel on 06.11.16.
   */
@@ -7,5 +9,10 @@ object NeuronNetworkUtils {
 
   def gaussFunction(x : Double, c : Double, r : Double) ={
     Math.exp(-(Math.pow(x-c, 2)/Math.pow(r,2)))
+  }
+
+  def generateWeightVector(size : Int): Array[Double] = {
+    val random = new Random()
+    Array.fill[Double](size)(random.nextDouble)
   }
 }
