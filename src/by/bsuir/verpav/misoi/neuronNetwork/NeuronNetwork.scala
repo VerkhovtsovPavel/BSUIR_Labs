636da29667e0d8d4958c6eee65f1ad5fc0e8262d
Pavel Verkhovtsov <verchpasha@gmail.com>
2016-11-15 18:02:55 +0300

    Add small  sample set

--- src/by/bsuir/verpav/misoi/neuronNetwork/NeuronNetwork.scala
+++ src/by/bsuir/verpav/misoi/neuronNetwork/NeuronNetwork.scala
@@ -6,10 +6,14 @@ import java.io.File
 import javax.imageio.ImageIO
 
 import by.bsuir.verpav.misoi.neuronNetwork.neurons.{HiddenNeuron, InputNeuron, OutputNeuron}
+import by.bsuir.verpav.misoi.neuronNetwork.NeuronNetworkUtils.calculateParametersGaussian
 import by.bsuir.verpav.misoi.util.ImageUtils
+import by.bsuir.verpav.misoi.util.ImageUtils.getPixelBrightness
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 class NeuronNetwork(sampleHeigth: Int, sampleWidth: Int) {
 
@@ -19,12 +23,11 @@ class NeuronNetwork(sampleHeigth: Int, sampleWidth: Int) {
 
   var classes: ArrayBuffer[String] = ArrayBuffer[String]()
 
-  def learn(className: String, sampleSet: List[Image]): Unit = {
-    val outputNeuron = new OutputNeuron(sampleSet.length)
+  def learn(className: String, sampleSet: List[BufferedImage]): Unit = {
+    val outputNeuron = new OutputNeuron(sampleHeigth * sampleWidth, className)
     outputNeurons.append(outputNeuron)
-    classes.append(className)
     for (x <- 0 until sampleWidth; y <- 0 until sampleHeigth) {
-      val (r, c) = calculateParametersGaussian(sampleSet, extractPixel(_: Image, x, y))
+      val (r, c) = calculateParametersGaussian(sampleSet, getPixelBrightness(_: BufferedImage, x, y))
       val hiddenNeuron = new HiddenNeuron(NeuronNetworkUtils.gaussFunction(_: Double, r, c))
       hiddenNeuron.addNeuronToNextLayer(outputNeuron)
       inputNeurons(x * sampleWidth + y).addNeuronToNextLayer(hiddenNeuron)
@@ -33,39 +36,43 @@ class NeuronNetwork(sampleHeigth: Int, sampleWidth: Int) {
 
   def learn(sampleSets: mutable.Map[String, List[File]]): Unit = {
     for (x <- 0 until sampleWidth; y <- 0 until sampleHeigth) {
-      inputNeurons(x * sampleWidth + y) = new InputNeuron[BufferedImage](extractPixel(_: BufferedImage, x, y))
+      inputNeurons(x * sampleWidth + y) = new InputNeuron[BufferedImage](getPixelBrightness(_: BufferedImage, x, y))
     }
 
-    val transformedSampleSet = sampleSets.map(ent => (ent._1, ent._2.map(ImageIO.read(_).getScaledInstance(sampleWidth, sampleHeigth, Image.SCALE_SMOOTH))))
+    val transformedSampleSet = sampleSets.map(ent => (ent._1, ent._2.map((i) => ImageUtils.imageBinarization(convertToBufferedImage(ImageIO.read(i).getScaledInstance(sampleWidth, sampleHeigth, Image.SCALE_SMOOTH))))))
     for (sampleSet <- transformedSampleSet) {
       learn(sampleSet._1, sampleSet._2)
     }
+
+    val zippedSet = new Random().shuffle(for (key <- transformedSampleSet.keys; value <- transformedSampleSet(key)) yield (key, value))
+
+    //colibrateWeightVectors(zippedSet, 0)
   }
 
   def classify(image: BufferedImage) : String = {
-    val preparedImage = convertToBufferedImage(image.getScaledInstance(sampleWidth, sampleHeigth, Image.SCALE_SMOOTH))
+    val preparedImage = ImageUtils.imageBinarization(convertToBufferedImage(image.getScaledInstance(sampleWidth, sampleHeigth, Image.SCALE_SMOOTH)))
     inputNeurons.foreach(_.pulse(preparedImage))
     val results = outputNeurons.map(_.getResult)
-    val className = classes(results.zipWithIndex.maxBy(_._1)._2)
+    val className = outputNeurons.zip(results).maxBy(_._2)._1.className
     className
   }
 
-  private def calculateParametersGaussian(sampleSet: List[Image], paramCalculator: Image => Double): (Double, Double) = {
-    val params = sampleSet.map(paramCalculator)
-    val r = params.max - params.min
-    val c = params.sum / params.length
-    (r, c)
+  @tailrec
+  private def colibrateWeightVectors(zippedSet: Iterable[(String, BufferedImage)], currentIteration: Int): Unit = {
+    var isFailed = false
+    for(sample <- zippedSet){
+      val prognosesClass = classify(sample._2)
+      if(prognosesClass!=sample._1){
+        isFailed = true
+        val correctClass = outputNeurons.filter(_.className==sample._1)
+        val incorrectClasses = outputNeurons.filter(_.className==prognosesClass)
+        correctClass.foreach(_.modifyWeights((x,y) => x+y, incorrectClasses.head.weights))
+        incorrectClasses.foreach(_.modifyWeights((x,y) => x-y, correctClass.head.weights))
+      }
+    }
+    if(currentIteration < 100 && isFailed)
+      colibrateWeightVectors(zippedSet, currentIteration+1)
   }
-
-  private def extractPixel(image: BufferedImage, x: Int, y: Int): Int = {
-    val pixel = ImageUtils.getPixel(image.getRaster, x, y)
-    ImageUtils.pointBrightness(pixel._1, pixel._2, pixel._3)
-  }
-
-  private def extractPixel(image: Image, x: Int, y: Int): Int = {
-    extractPixel(convertToBufferedImage(image), x, y)
-  }
-
 
   private def convertToBufferedImage(image: Image): BufferedImage = {
     val newImage = new BufferedImage(
