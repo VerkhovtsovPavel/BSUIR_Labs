d621253e10ff393c20488e0579b33bdab9ae66bd
Pavel Verkhovtsov <verchpasha@gmail.com>
2017-05-05 20:11:27 +0300

    Add text to development / Start diagrams

--- src/routing.scala
+++ src/routing.scala
@@ -1,55 +1,28 @@
 trait Service extends HttpService with ServiceJsonProtocol {
-  import scala.language.postfixOps
-  implicit def ec = actorRefFactory.dispatcher
+  ...
   def route(model: ActorRef)(implicit askTimeout: Timeout) =
-    get {
-      path("sample") {
-        onSuccess(model ? 'list) {
-          case item: List[Samples] =>
-            complete(OK, item)
-        }
-      } ~
-        path("sample" / IntNumber) { id =>
-          onSuccess(model ? id) {
-            case item: Sample =>
-              complete(OK, item)
-            case ItemNotFound =>
-              complete(NotFound, "Not Found")
-          }
-        }
-    } ~
-    post {
-      path("newSample") {
-        entity(as[ImageRequest]) {
-          image =>
-            onSuccess(model ? image) {
-              case id: Int =>
-                complete(OK, id.toString)
+    authorizeToken(verifyNotExpired && injectUser) {
+      user =>
+        get {
+            path("sample" / IntNumber) { id =>
+              onSuccess(model ? id) {
+                case item: Sample =>
+                  if(item.userId = user.uid)
+                    complete(OK, item)
+                  else
+                    complete(Forbidden, "Access denided")    
+                case ItemNotFound =>
+                  complete(NotFound, "Not Found")
+              }
             }
-        }
-      }
-    } ~
-    detete {
-      path("sample" / IntNumber) { id =>
-          onSuccess(model ? id) {
-            case item: Sample =>
-              complete(OK, item)
-            case ItemNotFound =>
-              complete(NotFound, "Not Found")
-          }
-      }
-    } ~
-    put {
-      path("sample" / IntNumber) {
-        entity(as[ImageRequest]) {
-          image =>
-            onSuccess(model ? image) {
-              case item: Sample =>
-                complete(OK, item)
-              case ItemNotFound =>
-                complete(NotFound, "Not Found")
-          }
-        }
-      }
-    }
+        } ~
+        post {
+          path("newSample") {
+            entity(as[ImageRequest]) {
+              image =>
+                onSuccess(model ? (image, user.uid)) {
+                  case id: Int =>
+                    complete(OK, id.toString)
+                }
+  ...    
 }
