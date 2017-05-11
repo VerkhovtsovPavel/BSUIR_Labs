a55dbef713a54d0ca29564a63b795953090ca6b7
Pavel Verkhovtsov <verchpasha@gmail.com>
2017-05-11 18:05:26 +0300

    Add Angular file upload

--- q.sc
+++
@@ -1,6 +0,0 @@
-import java.io.File
-
-import by.verkpavel.grafolnet.service.Segmentation.Segment
-
-val s = new Segment
-s.setImage(new File("~/Downloads/funny-english-language-jokes-14.jpg"))
--- src/main/scala/by/verkpavel/grafolnet/service/Service.scala
+++ src/main/scala/by/verkpavel/grafolnet/service/Service.scala
@@ -5,6 +5,7 @@ import akka.pattern.ask
 import akka.util.Timeout
 import by.verkpavel.grafolnet.model.ModelActor.ItemNotFound
 import by.verkpavel.grafolnet.model.{ImageRequest, ImageResponse}
+import spray.http.{MultipartFormData, StatusCodes}
 import spray.http.StatusCodes._
 import spray.routing.HttpService
 import spray.httpx.SprayJsonSupport._
@@ -25,14 +26,32 @@ trait Service extends HttpService with ServiceJsonProtocol {
 
   implicit def ec = actorRefFactory.dispatcher
 
+  case class ImageUploaded(size: Long)
+  implicit val imageUploadedFormat = jsonFormat1(ImageUploaded)
+
   def route(model: ActorRef)(implicit askTimeout: Timeout) =
     get {
-      path("imageParams") {
-        onSuccess(model ? 'list) {
-          case item: List[ImageResponse] =>
-            complete(OK, item)
-        }
+      path("index.html") {
+        getFromFile("src/main/angular/root/index.html")
       } ~
+        path("controllers.js") {
+          getFromFile("src/main/angular/root/controllers.js")
+        } ~
+        path("directives.js") {
+          getFromFile("src/main/angular/root/directives.js")
+        } ~
+        path("angular-file-upload.min.js") {
+          getFromFile("src/main/angular/root/angular-file-upload.min.js")
+        } ~
+        path("console-sham.js") {
+          getFromFile("src/main/angular/root/console-sham.js")
+        } ~
+        path("imageParams") {
+          onSuccess(model ? 'list) {
+            case item: List[ImageResponse] =>
+              complete(OK, item)
+          }
+        } ~
         path("imageParams" / IntNumber) { id =>
           onSuccess(model ? id) {
             case item: ImageResponse =>
@@ -44,13 +63,15 @@ trait Service extends HttpService with ServiceJsonProtocol {
         }
     } ~
       post {
-        path("newImage") {
-          entity(as[ImageRequest]) {
-            image =>
-              onSuccess(model ? image) {
-                case id: Int =>
-                  complete(OK, id.toString)
-              }
+        handleWith { data: MultipartFormData =>
+          data.fields.headOption match {
+            case Some(imageEntity) =>
+              val size = imageEntity.entity.data.length
+              println(s"Uploaded $size")
+              ImageUploaded(0)
+            case None =>
+              println("No files")
+              ImageUploaded(500)
           }
         }
       }
