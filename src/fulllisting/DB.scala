58e579838eba3d5408dc2fd402144a74a6c442a3
Pavel Verkhovtsov <verchpasha@gmail.com>
2017-10-11 20:05:40 +0300

    Graduated work as example

---
+++ src/fulllisting/DB.scala
@@ -0,0 +1,34 @@
+package by.verkpavel.grafolnet.database
+
+import java.awt.image.BufferedImage
+import java.io.{ByteArrayInputStream, File}
+import javax.imageio.ImageIO
+
+import by.verkpavel.grafolnet.database.dao.{SampleDAO, UserDAO}
+import by.verkpavel.grafolnet.database.domain.{Sample, SampleQueryParams, User, UserQueryParams}
+import com.mongodb.casbah.Imports._
+import by.verkpavel.grafolnet.database.dao.Conversions._
+
+/**
+ * Created by Pavel_Verkhovtsov on 4/11/17.
+ */
+object DB {
+  def getSampleByID(id: String) = sampleDAO.findOneById(new ObjectId(id)).get
+
+  val usersDAO = new UserDAO()
+  val sampleDAO = new SampleDAO()
+
+  def getImageParamsByID(id: String): Map[String, Double] = sampleDAO.findOneById(new ObjectId(id)).get.handwriteFeatures
+
+  def getImageByID(id: String): BufferedImage = ImageIO.read(new ByteArrayInputStream(getSampleByID(id).imageSource))
+
+  def getImages(userID: ObjectId): Seq[Sample] = sampleDAO.find(SampleQueryParams(user_id = Some(userID))).toList
+
+  def addSample(sample: Sample): String = sampleDAO.insert(sample).get.toHexString
+
+  def updateSample(sample: Sample) = sampleDAO.save(sample)
+
+  def addUser(user: User): String = usersDAO.insert(user).get.toHexString
+
+  def isUserExist(userName: String, password: String): Option[User] = usersDAO.findOne(UserQueryParams(name = Some(userName), passwordHash = Some(password)))
+}
---
+++ src/fulllisting/Service.scala
@@ -0,0 +1,57 @@
+package by.verkpavel.grafolnet.service
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.ask
+import akka.util.Timeout
+import by.verkpavel.grafolnet.model.ModelActor.ItemNotFound
+import by.verkpavel.grafolnet.model.{ImageRequest, ImageResponse}
+import spray.http.StatusCodes._
+import spray.routing.HttpService
+import spray.httpx.SprayJsonSupport._
+
+object ServiceActor {
+  def props(model: ActorRef)(implicit askTimeout: Timeout): Props = Props(classOf[ServiceActor], model, askTimeout)
+  def name = "service"
+}
+
+class ServiceActor(model: ActorRef, implicit val askTimeout: Timeout) extends Actor with Service {
+  def actorRefFactory = context
+  def receive = runRoute(route(model))
+}
+
+trait Service extends HttpService with ServiceJsonProtocol {
+
+  import scala.language.postfixOps
+
+  implicit def ec = actorRefFactory.dispatcher
+
+  def route(model: ActorRef)(implicit askTimeout: Timeout) =
+    get {
+      path("imageParams") {
+        onSuccess(model ? 'list) {
+          case item: List[ImageResponse] =>
+            complete(OK, item)
+        }
+      } ~
+        path("imageParams" / IntNumber) { id =>
+          onSuccess(model ? id) {
+            case item: ImageResponse =>
+              complete(OK, item)
+
+            case ItemNotFound =>
+              complete(NotFound, "Not Found")
+          }
+        }
+    } ~
+      post {
+        path("newImage") {
+          entity(as[ImageRequest]) {
+            image =>
+              onSuccess(model ? image) {
+                case id: Int =>
+                  complete(OK, id.toString)
+              }
+          }
+        }
+      }
+}
