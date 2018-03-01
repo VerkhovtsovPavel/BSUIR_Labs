e4f7ad92827f05224ae82e0344431a6e39b1a403
Pavel Verkhovtsov <verchpasha@gmail.com>
2018-03-01 17:41:00 +0300

    Update bootstrap version / Add initial sample service

---
+++ Web/angular2-jwt-authentication-example-master/app/models/sample.ts
@@ -0,0 +1,7 @@
+export class Sample {
+    id: 	string;
+    xs: 	number[];
+    ys: 	number[];
+    es:		number[];
+    times: 	number[];
+}
---
+++ Web/angular2-jwt-authentication-example-master/app/services/sample.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operator/map';
+
+import { AuthenticationService } from './authentication.service';
+import { Sample } from '../models/sample';
+
+@Injectable()
+export class SampleService {
+    constructor(
+        private http: Http,
+        private authenticationService: AuthenticationService) {
+    }
+
+    addSample(sample: Sample): Observable<Boolean> {
+        // add authorization header with jwt token
+        let headers = new Headers({ 'Token' : this.authenticationService.token, 'content-type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        // get users from api
+        return this.http.post('http://localhost:5467/v1/sample/new', JSON.stringify({ xs: sample.xs, ys: sample.ys, es: sample.es, times: sample.times }), options)
+            .map((response: Response) => response.json());
+    }
+}
