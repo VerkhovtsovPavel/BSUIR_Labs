5d1832795056793880705c64e627ec03fd19bad6
Pavel Verkhovtsov <verchpasha@gmail.com>
2019-01-07 01:01:46 +0300

    Fixed todos comments

---
+++ src/evaluate_single_instance.scala
@@ -0,0 +1,14 @@
+def predictPsychotype(feature: Features): Int {
+    real_feature_column = real_valued_column(...)
+    sparse_feature_column = sparse_column_with_hash_bucket(...)
+
+val estimator = SVM(
+    example_id_column='id',
+    feature_columns=[real_feature_column, sparse_feature_column],
+    l2_regularization=10.0)
+
+  ...
+
+val prediction = estimator.predict(feature)
+return formatPrediction(prediction)
+}
--- src/routing.scala
+++ src/routing.scala
@@ -1,28 +1,29 @@
-trait Service extends HttpService with ServiceJsonProtocol {
-  ...
-  def route(model: ActorRef)(implicit askTimeout: Timeout) =
-    authorizeToken(verifyNotExpired && injectUser) {
-      user =>
-        get {
-            path("sample" / IntNumber) { id =>
-              onSuccess(model ? id) {
-                case item: Sample =>
-                  if(item.userId = user.uid)
-                    complete(OK, item)
-                  else
-                    complete(Forbidden, "Access denided")    
-                case ItemNotFound =>
-                  complete(NotFound, "Not Found")
-              }
+  val route: Route = pathPrefix("samples") {
+    pathPrefix("new") {
+      pathEndOrSingleSlash {
+        authenticate(secretKey) { userId =>
+          post {
+            entity(as[Sample]) { sample =>
+              complete(createSample(sample).map {
+                case s: Sample =>
+                  OK -> s.asJson
+                case _ =>
+                  BadRequest -> None.asJson
+              })
             }
-        } ~
-        post {
-          path("newSample") {
-            entity(as[ImageRequest]) {
-              image =>
-                onSuccess(model ? (image, user.uid)) {
-                  case id: Int =>
-                    complete(OK, id.toString)
-                }
-  ...    
-}
+          }
+        }
+      }
+    } ~
+      pathPrefix(Segment) { id =>
+        pathEndOrSingleSlash {
+          get {
+            complete(getSample(id).map {
+              case Some(sample) =>
+                OK -> sample.asJson
+              case None =>
+                BadRequest -> None.asJson
+            })
+          }
+        }
+      }
