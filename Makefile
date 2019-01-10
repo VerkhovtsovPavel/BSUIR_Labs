ae29a5f3bcf3df27729c75aed567fd5a825a6124
Pavel Verkhovtsov <verchpasha@gmail.com>
2019-01-10 22:12:54 +0300

    Fixed all comments / Pre-release

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LATEXMK=latexmk
-DIPLOMA_SRC=autoref.tex
+DIPLOMA_SRC=dissertation.tex
 
 all: dissertation
 
--- src/routing.scala
+++ src/routing.scala
@@ -1,29 +1,6 @@
-  val route: Route = pathPrefix("samples") {
-    pathPrefix("new") {
-      pathEndOrSingleSlash {
+        ...
         authenticate(secretKey) { userId =>
           post {
             entity(as[Sample]) { sample =>
               complete(createSample(sample).map {
-                case s: Sample =>
-                  OK -> s.asJson
-                case _ =>
-                  BadRequest -> None.asJson
-              })
-            }
-          }
-        }
-      }
-    } ~
-      pathPrefix(Segment) { id =>
-        pathEndOrSingleSlash {
-          get {
-            complete(getSample(id).map {
-              case Some(sample) =>
-                OK -> sample.asJson
-              case None =>
-                BadRequest -> None.asJson
-            })
-          }
-        }
-      }
+        ...      
